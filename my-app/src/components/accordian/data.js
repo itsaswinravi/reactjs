706b98917b0f28376fd56d3ecaf571aea3fe1b10
itsaswinravi <achu322001@gmail.com>
2024-04-03 11:31:31 +0530

    11

---
+++ my-app/src/components/accordian/data.js
@@ -0,0 +1,18 @@
+const data =[
+    {
+        id : '1',
+        question: "what are accordian components?",
+        answer: "Accordian components are user interface elements used for organisations"
+    },
+    {
+        id : '2',
+        question: "what are they used for?",
+        answer: "they are commonly employed in various contexts,including FAQs, product description "
+    },
+    {
+        id : '3',
+        question: "Accordian as a musical instrument",
+        answer : "The accordian is a musical instrument with akeyboard and bellows" 
+    }
+];
+export default data;
