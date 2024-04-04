a553c778d13cb7d2b95e3d8b36bda592ebc29f40
itsaswinravi <achu322001@gmail.com>
2024-04-04 11:22:05 +0530

    2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
+import ImageSlider from './components/image-slider';
 
 function App() {
   return (
@@ -11,7 +12,9 @@ function App() {
 
       {/* <RandomColor/> */}
       
-      <StarRating noOfStars={10}/>
+      {/* <StarRating noOfStars={10}/> */}
+
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"4"}/>
     </div>
   );
 }
