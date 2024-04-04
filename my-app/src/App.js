8dd9c17e82151b1e1dda2651e45856065597ccd6
itsaswinravi <achu322001@gmail.com>
2024-04-04 15:04:02 +0530

    4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,7 @@ import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
 import TreeView from './components/tree-views';
 import menus from "./components/tree-views/data"
+import QRCodeGenerator from './components/qr-code-generator';
 
 function App() {
   return (
@@ -19,7 +20,8 @@ function App() {
 
       {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"4"}/> */}
       {/* <LoadMoreData /> */}
-      <TreeView menus={menus} />
+      {/* <TreeView menus={menus} /> */}
+      <QRCodeGenerator />
     </div>
   );
 }
