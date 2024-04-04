4a131b7c3f80947beeaa45df7e855c52c7b2963f
itsaswinravi <achu322001@gmail.com>
2024-04-04 11:59:55 +0530

    3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
       
       {/* <StarRating noOfStars={10}/> */}
 
-      <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"4"}/>
+      {/* <ImageSlider url={"https://picsum.photos/v2/list"} page={'1'} limit={"4"}/> */}
+      <LoadMoreData />
     </div>
   );
 }
